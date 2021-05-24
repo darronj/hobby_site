0071eb7f7e3ae3ea64e8adf9b5657c0c70eee468
Darron Jennings <darronj@outlook.com>
2021-05-24 11:40:04 -0500

    updating config settings.

--- next.config.js
+++ next.config.js
@@ -9,11 +9,12 @@ const getURL = () => {
       : 'http://localhost:3000';
   return url.includes('http') ? url : `https://${url}`;
 };
+const URL = getURL();
 
 module.exports = {
   env: {
     NEXT_PUBLIC_SITE_NAME: process.env.SITE_NAME || 'My Site',
-    NEXT_PUBLIC_URL: getURL(),
-    AUTH0_BASE_URL: getURL()
+    NEXT_PUBLIC_URL: URL,
+    AUTH0_BASE_URL: URL
   }
 };
